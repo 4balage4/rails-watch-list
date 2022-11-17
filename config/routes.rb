f6f553c422c61bdedf60cce8002a2c1b95adf0b3
balazs burian <bur.balazs@protonmail.com>
2022-11-17 17:20:28 +0100

    movie list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'list#index'
+#  resources :lists
 
+  resources :lists do
+    resources :bookmarks, only: [:index, :new, :create]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
-end
+  # get 'lists', to: 'lists#index'
+  # get 'lists/new', to: 'lists#new', as: 'new'
+  # get 'lists/:id', to: 'lists#show'
+  # post 'lists', to: 'lists#create'
+
+  resources :bookmarks, only: :destroy
+  end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# 10.times do |movie|
+#   randomnumber = [1, 2, 3, 4, 5].sample
+#   movie = Movie.create(title: Faker::Movie.title, overview: Faker::Movie.quote, poster_url: Faker::Internet.url, rating: randomnumber )
+#   puts movie.title
+
+#   end
+    
+
+    List.create(name: "romantic")
+    List.create(name: "horror")
+    List.create(name: "badmovie")
+    List.create(name: "classic")
